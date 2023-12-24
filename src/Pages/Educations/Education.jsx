e68fe0e18d1b5b73ecd2ce5862a87daa303ee647
MdAkbar05 <samratakbar667466@gmail.com>
2023-12-24 22:51:13 +0600

    updated

--- src/Pages/Educations/Education.jsx
+++ src/Pages/Educations/Education.jsx
@@ -10,7 +10,7 @@ const Education = () => {
     <Container className="Education">
       <Fade left>
         <h1 className="center">
-          <img src={EduLogo} width={65} height={60} alt="Educations" />
+          <img src={EduLogo} width={60} height={60} alt="Educations" />
           <span className="ht">Education Qualifications</span>
         </h1>
       </Fade>
@@ -23,7 +23,7 @@ const Education = () => {
           <Zoom>
             <div className="degree">
               <CheckCircleRoundedIcon style={{ marginRight: "10px" }} />
-              Secoundary School Certificates | 2020
+              Secondary School Certificates | 2020
             </div>
             <div className="degree">
               <CheckCircleRoundedIcon style={{ marginRight: "10px" }} /> Diploma
